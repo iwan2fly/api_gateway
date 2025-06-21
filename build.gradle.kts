4094bc1037f8e0dcb3c9da50442a798d697ba27e
김신강 <iwan2fly@gmail.com>
2025-06-21 16:32:58 +0900

    대충 커밋

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,18 +17,26 @@ repositories {
 	mavenCentral()
 }
 
+extra["springCloudVersion"] = "2025.0.0"
+
 dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    
+
     // 로깅
-    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
-    
+    // implementation("net.logstash.logback:logstash-logback-encoder:7.4")
+
     // 개발용 도구
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    
+
     // 테스트
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
 
 tasks.withType<Test> {
